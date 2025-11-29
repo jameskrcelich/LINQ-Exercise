7e991b8336ca7aa1efd34faef9571ae52992dab5
James Krcelich <james.krcelich@sbcglobal.net>
2025-11-28 19:22:25 -0600

    final changes

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("NBA Teams");
+            
+            List<string> nbaEasternConferenceTeams = new List<string>()
+            {
+                "Chicago Bulls", "Detroit Pistons", "Cleveland Cavaliers",
+                "Toronto Raptors", "Miami Heat", "New York Knicks", "Atlanta Hawks",
+                "Orlando Magic", "Boston Celtics", "Philadelphia 76'ers", "Milwaukee Bucks",
+                "Charlotte Hornets", "New Jersey Nets", "Washington Wizards", "Indiana Pacers"
+            };
+            
+            // List the teams by length of name - shortest to longest
+            var orderedNbaTeams = nbaEasternConferenceTeams.OrderBy(n => n.Length);
+            foreach (var nbaTeam in orderedNbaTeams)
+            {
+                Console.WriteLine(nbaTeam);
+            }
         }
     }
 }
